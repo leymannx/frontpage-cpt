2f14c1521a8437d6d082578bd90c500287e971a5
Norman Kämper-Leymann <norman.leymann@gmail.com>
2016-06-10 12:36:37 +0200

    added raw custom fields (draft)

--- frontpage-cpt.php
+++ frontpage-cpt.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name: Front Page Custom Post Type
 Plugin URI: https://github.com/leymannx/wordpress-custom-post-type.git
-Description: This WordPress plugin creates a custom post type Front Page with some custom fields. Additionally it makes Front Page posts available to choose as static WordPress front page under <a href="/wp-admin/options-reading.php">/wp-admin/options-reading.php</a>. Multi-language support enabled.
+Description: This WordPress plugin creates a custom post type Front Page with some custom fields. Additionally it makes Front Page posts available to choose as static WordPress front page under <a href='/wp-admin/options-reading.php'>/wp-admin/options-reading.php</a>. Multi-language support enabled.
 Version: 1.0
 Author: Norman Kämper-Leymann
 Author URI: http://berlin-coding.de
@@ -54,7 +54,7 @@ function frontpage_cpt_create() {
 		'menu_icon'           => 'dashicons-welcome-view-site',
 		'capability_type'     => 'page',
 		'hierarchical'        => FALSE,
-		'supports'            => array( 'title', 'editor', 'thumbnail', 'custom-fields', 'revisions' ),
+		'supports'            => array( 'title', 'editor', 'thumbnail', /*'custom-fields', 'revisions'*/ ),
 		'has_archive'         => FALSE,
 		'rewrite'             => array( 'slug' => $slug ),
 		'query_var'           => FALSE, // rabbit hole
@@ -108,3 +108,63 @@ function frontpage_cpt_enable_frontpage( $query ) {
 }
 
 add_action( 'pre_get_posts', 'frontpage_cpt_enable_frontpage' );
+
+function admin_init() {
+
+	add_meta_box( $id = 'year_completed-meta', $title = 'Year Completed', $callback = 'year_completed', $screen = 'frontpage_cpt', $context = 'side', $priority = 'low', $callback_args = NULL );
+	add_meta_box( $id = 'credits_meta', $title = 'Design & Build Credits', $callback = 'credits_meta', $screen = 'frontpage_cpt', $context = 'normal', $priority = 'low', $callback_args = NULL );
+//	add_meta_box( $id, $title, $callback, $page, $context, $priority );
+//	add_meta_box ( $id, $title, $callback, $screen = null, $context = 'advanced', $priority = 'default', $callback_args = null );
+//	add_meta_box( 'hello', 'Hello Hello', 'hello_callback', $screen = NULL, $context = 'advanced', $priority = 'default', $callback_args = NULL );
+}
+
+function hello_callback() {
+
+}
+
+function year_completed() {
+
+	global $post;
+	$custom = get_post_custom( $post->ID );
+	$year_completed = $custom['year_completed'][0];
+	?>
+	<label>Year:</label>
+	<input name='year_completed' value='<?php echo $year_completed; ?>'/>
+	<?php
+}
+
+function credits_meta() {
+
+	global $post;
+	$custom = get_post_custom( $post->ID );
+	$designers = $custom['designers'][0];
+	$developers = $custom['developers'][0];
+	$producers = $custom['producers'][0];
+	?>
+	<p><label>Designed By:</label><br/>
+		<?php wp_editor( $content = $designers, $editor_id = 'mettaabox_ID_stylee', $settings = array(
+			'textarea_name' => 'designers',
+			'media_buttons' => FALSE,
+			'textarea_rows' => 5,
+
+		) ); ?></p>
+	<p><label>Built By:</label><br/>
+		<textarea cols='50' rows='5' name='developers'><?php echo $developers; ?></textarea></p>
+	<p><label>Produced By:</label><br/>
+		<textarea cols='50' rows='5' name='producers'><?php echo $producers; ?></textarea></p>
+	<?php
+}
+
+add_action( 'admin_init', 'admin_init' );
+
+function save_details() {
+
+	global $post;
+
+	update_post_meta( $post->ID, 'year_completed', $_POST['year_completed'] );
+	update_post_meta( $post->ID, 'designers', $_POST['designers'] );
+	update_post_meta( $post->ID, 'developers', $_POST['developers'] );
+	update_post_meta( $post->ID, 'producers', $_POST['producers'] );
+}
+
+add_action( 'save_post', 'save_details' );
